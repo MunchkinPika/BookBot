0e4b518496aa887d686c8769e759021fc97add98
ThePrimeagen <the.primeagen@aol.com>
2025-05-15 22:45:00 -0400

    C: Refactoring

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ def get_book_text(book):
     with open(book) as f:
         file_contents = f.read()
         return file_contents
-def count_words(text):  
-    words = text.split()
-    num_words = len(words)
-    print(f"{num_words} words found in the document")
+
+from stats import count_words
 
 def main (): 
     #relative path to book 
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(text):  
+    words = text.split()
+    num_words = len(words)
+    print(f"{num_words} words found in the document")

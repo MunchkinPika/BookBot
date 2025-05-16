dba56b7fbe5f7d3b4c8a2cac507f5a0fc77b9a11
ThePrimeagen <the.primeagen@aol.com>
2025-05-15 22:57:53 -0400

    D: Adding character count

--- main.py
+++ main.py
@@ -7,12 +7,20 @@ def get_book_text(book):
         return file_contents
 
 from stats import count_words
+from stats import count_characters
 
 def main (): 
     #relative path to book 
     path_to_book = 'books/frankenstein.txt'
-    text = get_book_text(path_to_book)
-    count_words(text)
+    text = get_book_text(path_to_book) 
 
+  # Count and print number of words
+    count_words(text)
+    
+    # Count characters
+    character_frequencies = count_characters(text)
+    
+    for i, (char, count) in enumerate(character_frequencies.items()):
+        print(f"'{char}': {count}")
 
 main ()
--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@ def count_words(text):
     words = text.split()
     num_words = len(words)
     print(f"{num_words} words found in the document")
+    
+def count_characters(text):
+    text = text.lower()
+    char_freq = {}
+    
+    for char in text:
+        if char in char_freq:
+            char_freq[char] += 1
+        else:
+            char_freq[char] = 1
+
+    return char_freq

8c9e1c68dfb4c3cc67601ba751000010a4c34356
ThePrimeagen <the.primeagen@aol.com>
2025-05-15 22:40:08 -0400

    B: Adding count words

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ def get_book_text(book):
     with open(book) as f:
         file_contents = f.read()
         return file_contents
+def count_words(text):  
+    words = text.split()
+    num_words = len(words)
+    print(f"{num_words} words found in the document")
 
 def main (): 
     #relative path to book 
     path_to_book = 'books/frankenstein.txt'
     text = get_book_text(path_to_book)
-    print(text)
+    count_words(text)
+
 
 main ()
